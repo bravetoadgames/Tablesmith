f35aa0664028059feb80c100d41e8612b02a0350
Arjen Schumacher <arjen.schumacher@gmail.com>
2026-02-16 09:56:13 +0100

    First commit

---
+++ tablesmith.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Tablesmith
+
+A fun RPG random table generator for anything you like.
+
+Created on Sun Feb 15 18:17:59 2026
+
+@author: arjeneke
+"""
+
+from classes.class_overview_location import OverviewLocation
+
+overview_location = OverviewLocation()
+
